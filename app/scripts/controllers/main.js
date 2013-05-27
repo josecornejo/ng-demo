ad2cf35d7b2ac7932a0d50dba4d7887dfb5be393
Jose Cornejo <josecornejo.tj@gmail.com>
2013-05-27 15:34:47 -0700

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngDemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
